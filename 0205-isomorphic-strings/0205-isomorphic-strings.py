51a510123cf757198191fe4c6c35e0cacdb2b75b
NuhaminBaye <167539301+NuhaminBaye@users.noreply.github.com>
2025-01-07 09:35:15 -0800

    Time: 7 ms (59.04%), Space: 18 MB (21.76%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        # Create two dictionaries for mapping characters
+        s_to_t = {}
+        t_to_s = {}
+        
+        for char_s, char_t in zip(s, t):
+            # Check the mapping from s to t
+            if char_s in s_to_t:
+                if s_to_t[char_s] != char_t:
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+            
+            # Check the mapping from t to s
+            if char_t in t_to_s:
+                if t_to_s[char_t] != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+        
+        return True
